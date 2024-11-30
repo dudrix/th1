0ac76699ff8a460d0a6bce3d8a681fc7bc4e086e
dudrix <138270121+dudrix@users.noreply.github.com>
2024-11-30 16:59:18 +0000

    🫦

--- WORK/tcpdumptest.sh
+++ WORK/tcpdumptest.sh
@@ -1,2 +1,21 @@
 #!/usr/bin/bash
-set -euo 
+set -euo pipefail 
+
+# colors 
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+YELLOW='\033[0;33m'
+BLUE='\033[0;34m'
+NC='\033[0m' # No Color
+
+# Commands
+b1() {
+    echo -e "${GREEN}Command: ${YELLOW}tcpdump -i docker0 -w test.pcap${NC}"
+}
+
+
+c1() {
+    tcpdump -i docker0 -w test.pcap
+}
+
+# execution
